ce086a842f7b20933f85c3b672c7f13d17dde5ca
Pavel Perestoronin <eigenein@gmail.com>
2013-03-18 23:57:42 +0300

    ClusterOverlay: print SSID if it is the only one, more pretty text

--- src/info/eigenein/openwifi/helpers/map/ClusterOverlay.java
+++ src/info/eigenein/openwifi/helpers/map/ClusterOverlay.java
@@ -12,32 +12,49 @@ import info.eigenein.openwifi.helpers.entities.Cluster;
 import info.eigenein.openwifi.helpers.location.L;
 
 public class ClusterOverlay extends Overlay {
-    private static final float TEXT_OFFSET = 8.0f;
+    private static final float TEXT_OFFSET = 12.0f;
 
+    private static final Paint circlePaint = new Paint();
     private static final Paint defaultPaint = new Paint();
+    private static final Paint strokePaint = new Paint();
 
-    private final String clusterSizeString;
-
-    private final GeoPoint geoPoint;
-
-    private final Area area;
+    private final GeoPoint clusterCenter;
+    private final Area clusterArea;
 
     private final Bitmap clusterBitmap;
+    private final String clusterSizeString;
 
     static
     {
+        defaultPaint.setAntiAlias(true);
+        defaultPaint.setColor(Color.BLACK);
         defaultPaint.setTextSize(16.0f);
+        defaultPaint.setTypeface(Typeface.DEFAULT_BOLD);
+
+        strokePaint.setAntiAlias(true);
+        strokePaint.setColor(Color.WHITE);
+        strokePaint.setStrokeWidth(2);
+        strokePaint.setStyle(Paint.Style.STROKE);
+        strokePaint.setTextSize(16.0f);
+        strokePaint.setTypeface(Typeface.DEFAULT_BOLD);
+
+        circlePaint.setAntiAlias(true);
+        circlePaint.setColor(Color.BLACK);
+        circlePaint.setAlpha(32);
+        circlePaint.setStyle(Paint.Style.FILL);
     }
 
     public ClusterOverlay(Context context, Cluster cluster) {
         this.clusterBitmap = BitmapFactory.decodeResource(
                 context.getResources(),
                 R.drawable.ic_cluster);
-        this.clusterSizeString = Integer.toString(cluster.size());
-        this.area = cluster.getArea();
-        this.geoPoint = new GeoPoint(
-                L.toE6(area.getLatitude()),
-                L.toE6(area.getLongitude()));
+        this.clusterSizeString = cluster.size() == 1 ?
+                cluster.iterator().next().getSsid() :
+                Integer.toString(cluster.size());
+        this.clusterArea = cluster.getArea();
+        this.clusterCenter = new GeoPoint(
+                L.toE6(clusterArea.getLatitude()),
+                L.toE6(clusterArea.getLongitude()));
     }
 
     @Override
@@ -52,17 +69,11 @@ public class ClusterOverlay extends Overlay {
         final Projection projection = mapView.getProjection();
 
         Point point = new Point();
-        projection.toPixels(geoPoint, point);
+        projection.toPixels(clusterCenter, point);
 
         final float radius = (float)(
-                projection.metersToEquatorPixels(area.getAccuracy()) *
-                        (1.0 / Math.cos(Math.toRadians(area.getLatitude()))));
-
-        Paint circlePaint = new Paint();
-        circlePaint.setColor(Color.BLACK);
-        circlePaint.setAlpha(32);
-        circlePaint.setAntiAlias(true);
-        circlePaint.setStyle(Paint.Style.FILL);
+                projection.metersToEquatorPixels(clusterArea.getAccuracy()) *
+                        (1.0 / Math.cos(Math.toRadians(clusterArea.getLatitude()))));
 
         final float x = (float)point.x;
         final float y = (float)point.y;
@@ -73,6 +84,8 @@ public class ClusterOverlay extends Overlay {
                 x - clusterBitmap.getWidth() / 2.0f,
                 y - clusterBitmap.getHeight() / 2.0f,
                 defaultPaint);
+
+        canvas.drawText(clusterSizeString, x + TEXT_OFFSET, y - TEXT_OFFSET, strokePaint);
         canvas.drawText(clusterSizeString, x + TEXT_OFFSET, y - TEXT_OFFSET, defaultPaint);
     }
 }
