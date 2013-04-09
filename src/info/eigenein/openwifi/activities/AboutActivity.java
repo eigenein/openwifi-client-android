b728ad7bee962efd570ffc9fe981317c58795697
Pavel Perestoronin <eigenein@gmail.com>
2013-04-09 04:51:28 +0300

    #62 Version

--- src/info/eigenein/openwifi/activities/AboutActivity.java
+++ src/info/eigenein/openwifi/activities/AboutActivity.java
@@ -1,21 +1,27 @@
 package info.eigenein.openwifi.activities;
 
 import android.app.Activity;
-import android.os.Build;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import info.eigenein.openwifi.R;
 
 public class AboutActivity extends Activity {
-    @Override
+    private static String LOG_TAG = AboutActivity.class.getCanonicalName();
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.about);
 
-        // Setup action bar.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            getActionBar().setDisplayShowTitleEnabled(false);
+        // Update version name text view.
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            TextView versionTextView = (TextView)findViewById(R.id.about_version_text_view);
+            versionTextView.setText(String.format(getString(R.string.version), versionName));
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(LOG_TAG, e.toString());
         }
     }
 
