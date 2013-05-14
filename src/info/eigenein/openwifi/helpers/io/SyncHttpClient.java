d5aec0590057f58975e2d12cfe92958ec7e1f794
Pavel Perestoronin <eigenein@gmail.com>
2013-05-15 00:43:57 +0300

    #66 Клиентский сертификат

--- src/info/eigenein/openwifi/helpers/io/SyncHttpClient.java
+++ src/info/eigenein/openwifi/helpers/io/SyncHttpClient.java
@@ -15,17 +15,21 @@ import java.io.InputStream;
 import java.security.KeyStore;
 
 public class SyncHttpClient extends DefaultHttpClient {
-    private final static HttpParams httpParams = new BasicHttpParams();
+    private final static char[] TRUST_STORE_PASSWORD = "St6qe5en".toCharArray();
+
+    private final static String KEY_STORE_PASSWORD = "rkJNiD2Mew9mYBo1";
+
+    private final static HttpParams HTTP_PARAMS = new BasicHttpParams();
 
     private final Context context;
 
     static {
         // Use HTTP 1.1.
-        HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
+        HttpProtocolParams.setVersion(HTTP_PARAMS, HttpVersion.HTTP_1_1);
     }
 
     public SyncHttpClient(Context context) {
-        super(httpParams);
+        super(HTTP_PARAMS);
 
         this.context = context;
 
@@ -40,21 +44,36 @@ public class SyncHttpClient extends DefaultHttpClient {
      */
     private SSLSocketFactory createSslSocketFactory() {
         try {
-            // Initialize the trust store.
-            final KeyStore trustStore = KeyStore.getInstance("BKS");
-            // Load the resource with the trust store.
+            // Load the key and trust stores.
+            final KeyStore keyStore = loadKeyStore(R.raw.key_store, KEY_STORE_PASSWORD.toCharArray());
+            final KeyStore trustStore = loadKeyStore(R.raw.trust_store, TRUST_STORE_PASSWORD);
+            // Create and return the SSL socket factory.
+            return new SSLSocketFactory(keyStore, KEY_STORE_PASSWORD, trustStore);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create SSL socket factory.", e);
+        }
+    }
+
+    /**
+     * Loads the key store from the specified resource.
+     */
+    private KeyStore loadKeyStore(final int resourceId, final char[] password) {
+        try {
+            // Initialize the key store.
+            final KeyStore keyStore = KeyStore.getInstance("BKS");
+            // Load the resource with the key store.
             final InputStream trustStoreInputStream =
-                    context.getResources().openRawResource(R.raw.trust_store);
+                    context.getResources().openRawResource(resourceId);
             // Load the key store.
             try {
-                trustStore.load(trustStoreInputStream, "St6qe5en".toCharArray());
+                keyStore.load(trustStoreInputStream, password);
             } finally {
                 trustStoreInputStream.close();
             }
-            // Create the socket factory with the specified trust store.
-            return new SSLSocketFactory(trustStore);
+            // Done.
+            return keyStore;
         } catch (Exception e) {
-            throw new RuntimeException("Failed to create SSL socket factory.", e);
+            throw new RuntimeException("Failed to load the key store..", e);
         }
     }
 }
