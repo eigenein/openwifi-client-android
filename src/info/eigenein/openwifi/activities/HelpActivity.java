cf4d3446275d5c7f1c79226531e666aa1408b2ad
Pavel Perestoronin <eigenein@gmail.com>
2013-05-25 01:30:32 +0300

    #35 Пустые табы

--- src/info/eigenein/openwifi/activities/HelpActivity.java
+++ src/info/eigenein/openwifi/activities/HelpActivity.java
@@ -1,17 +1,65 @@
 package info.eigenein.openwifi.activities;
 
-import android.app.Activity;
+import android.app.*;
 import android.os.Bundle;
+import android.support.v4.view.*;
 import android.view.*;
 import com.google.analytics.tracking.android.*;
+import info.eigenein.openwifi.*;
 import info.eigenein.openwifi.helpers.*;
+import info.eigenein.openwifi.helpers.ui.*;
+
+import java.util.*;
 
 public class HelpActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.help);
+
+        final LayoutInflater inflater = LayoutInflater.from(this);
+        final List<View> pages = Arrays.asList(
+                inflater.inflate(R.layout.help_welcome, null),
+                inflater.inflate(R.layout.help_scan, null)
+        );
+
+        final ViewPager viewPager = new ViewPager(this);
+        viewPager.setAdapter(new ListPagerAdapter(pages));
+        viewPager.setCurrentItem(0); // TODO: extract current item from bundle.
+
+        setContentView(viewPager);
+
         if (BuildHelper.isHoneyComb()) {
-            getActionBar().setDisplayHomeAsUpEnabled(true);
+            final ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            // Specify that tabs should be displayed in the action bar.
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            // Create a tab listener that is called when the user changes tabs.
+            final ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+                public void onTabSelected(final ActionBar.Tab tab, final FragmentTransaction ft) {
+                    viewPager.setCurrentItem(tab.getPosition());
+                }
+
+                public void onTabUnselected(final ActionBar.Tab tab, final FragmentTransaction ft) {
+                    // Do nothing.
+                }
+
+                public void onTabReselected(final ActionBar.Tab tab, final FragmentTransaction ft) {
+                    // Do nothing.
+                }
+            };
+            // Add the tabs, specifying the tab's text and TabListener.
+            for (int i = 0; i < 2; i++) {
+                actionBar.addTab(actionBar.newTab().setText("Tab " + (i + 1)).setTabListener(tabListener));
+            }
+            // Select the corresponding tab when the user swipes between pages with a touch gesture.
+            viewPager.setOnPageChangeListener(
+                    new ViewPager.SimpleOnPageChangeListener() {
+                        @Override
+                        public void onPageSelected(int position) {
+                            getActionBar().setSelectedNavigationItem(position);
+                        }
+                    });
         }
     }
 
